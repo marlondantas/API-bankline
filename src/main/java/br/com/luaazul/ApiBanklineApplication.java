702b2dd5f5c2737624d747250a7d681e8628fefc
Marlon Dantas <marlondantas2010@gmail.com>
2022-05-20 10:30:32 -0300

    fist commit

---
+++ src/main/java/br/com/luaazul/ApiBanklineApplication.java
@@ -0,0 +1,13 @@
+package br.com.luaazul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBanklineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBanklineApplication.class, args);
+	}
+
+}
